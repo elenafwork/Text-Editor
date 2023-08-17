876baa7efac9adf1dd6c970e4f6206a9cd428fa6
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-08-17 12:46:48 -0500

    webpack, sw, icon ,

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        inject: true,
+        background_color: '#fff',
+        theme_color: "#fff",
+        description: `Text editor`,
+        icons: [
+          {
+            destination: path.join('assets', 'icons'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            src: path.resolve("src/images/logo.png"),
+          },
+        ],
+        name: `Text Editor`,
+        short_name: 'Text Editor',
+        start_url: '/',
+        publicPath: '/'
+
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
